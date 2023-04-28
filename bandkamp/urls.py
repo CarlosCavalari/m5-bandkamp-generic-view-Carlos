ae67a2b16ec1bbbd36c17756f7b284bae70d063a
Carlos Eduardo <carlospcavalari@gmail.com>
2023-04-27 21:48:41 -0300

    fix: docs

--- bandkamp/urls.py
+++ bandkamp/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("albums.urls")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema")),
     path("api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema")),
 ]
